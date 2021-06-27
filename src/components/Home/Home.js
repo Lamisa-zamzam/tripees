3e437167f1cae0b94deef5730efba9a41f5e2312
Your Name <lamisazamzam@gmail.com>
2021-06-27 12:41:38 +0600

    hello

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,27 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Home = () => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Methods":
+                "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "86400",
+        },
+
+        body: {
+            ClientId: "ApiIntegrationNew",
+            UserName: "HARE",
+            Password: "HARE@1234",
+            LoginType: "2",
+            EndUserIp: "103.195.184.237",
+        },
+    };
     const [hotels, setHotels] = useState(null);
+
     useEffect(() => {
         fetch(
             "http://api.tektravels.com/SharedServices/SharedData.svc/rest/Authenticate",
@@ -22,6 +42,13 @@ const Home = () => {
             .then((res) => {
                 res.json();
             })
+            .then((data) => console.log(data));
+
+        axios
+            .post(
+                "http://api.tektravels.com/SharedServices/SharedData.svc/rest/Authenticate",
+                options
+            )
             .then((data) => console.log(data));
     }, []);
     return (
